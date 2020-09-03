e027d59c4b5694786b9b249b3a16463c9d3d88b5
suleiman-oss <69819485+suleiman-oss@users.noreply.github.com>
2020-09-03 18:02:06 +0530

    Update Tic_Tac_Toe.py

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -15,14 +15,14 @@ def evaluate(state):
         score=0
     return score
 def wins(state, player):
-    win_state=[[state[0][0],state[0][1],state[0][2]],
-               [state[1][0],state[1][1],state[1][2]],
-               [state[2][0],state[2][1],state[2][2]],
-               [state[0][0],state[1][0],state[2][0]],
-               [state[0][1],state[1][1],state[2][1]],
-               [state[0][2],state[1][2],state[2][2]],
-               [state[0][0],state[1][1],state[2][2]],
-               [state[0][2],state[1][1],state[2][1]]]
+    win_state = [[state[0][0], state[0][1], state[0][2]],
+        [state[1][0], state[1][1], state[1][2]],
+        [state[2][0], state[2][1], state[2][2]],
+        [state[0][0], state[1][0], state[2][0]],
+        [state[0][1], state[1][1], state[2][1]],
+        [state[0][2], state[1][2], state[2][2]],
+        [state[0][0], state[1][1], state[2][2]],
+        [state[2][0], state[1][1], state[0][2]],]
     if [player,player,player] in win_state:
         return True
     else:
